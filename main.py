836feae40454258e83b4abc65546148db2966dbd
lingl <lingphoonlee@gmail.com>
2021-08-15 13:20:35 -0600

    Add .env file, update requirements.txt, add movies to postgres database

--- main.py
+++ main.py
@@ -18,14 +18,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DecimalField, IntegerField
 from wtforms.validators import DataRequired
 import requests
+import os
+from dotenv import load_dotenv
 
-MOVIE_API_KEY = '452fa104bf9fd5e158ec2a535469a28e'
+load_dotenv('.env')
+try:
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+except:
+    MOVIE_API_KEY = os.getenv('MOVIE_API_KEY')
 SEARCH_API_URL = 'https://api.themoviedb.org/3/search/movie'
 MOVIE_DB_IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my-movies.db'
+try:
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI_FIXED', 'sqlite:///my-movies.db')
+except:
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI_FIXED', 'sqlite:///my-movies.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
 
@@ -58,15 +68,63 @@ class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False, unique=True)
     year = db.Column(db.Integer)
-    description = db.Column(db.String(250))
+    description = db.Column(db.String(500))
     rating = db.Column(db.Integer)
     ranking = db.Column(db.Integer)
-    review = db.Column(db.String(250))
-    img_url = db.Column(db.String(250))
+    review = db.Column(db.String(500))
+    img_url = db.Column(db.String(500))
+
 
 try:
     db.create_all()
+    movie1 = Movies(id=1,
+                    title='千と千尋の神隠し',
+                    year=2001,
+                    description='A young girl, Chihiro, becomes trapped in a strange new world of spirits. When her parents undergo a mysterious transformation, she must call upon the courage she never knew she had to free her family.',
+                    rating=9,
+                    ranking=5,
+                    review='This was my favorite movie from my childhood.',
+                    img_url='https://image.tmdb.org/t/p/w500//39wmItIWsg5sZMyRUHLkWBcuVCM.jpg')
+    movie2 = Movies(id=2,
+                    title='君の膵臓をたべたい',
+                    year=2018,
+                    description='Living with Dying." He soon discovers that it is a diary kept by his genuinely cheerful classmate, Sakura Yamauchi, who reveals to him that she is secretly suffering from a pancreatic illness and only has a limited time left.',
+                    rating=10,
+                    ranking=1,
+                    review='This is one of my favorite movies that I\'ve seen in the past decade.',
+                    img_url='https://image.tmdb.org/t/p/w500//vHdVU0HyyB6k6kuYt8qjwTz9one.jpg')
+    movie3 = Movies(id=3,
+                    title='The Martian',
+                    year=2015,
+                    description='During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet.',
+                    rating=8,
+                    ranking=4,
+                    review='This is a non-stop on the edge of your seat type of movie.',
+                    img_url='https://image.tmdb.org/t/p/w500//5BHuvQ6p9kfc091Z8RiFNhCwL4b.jpg')
+    movie4 = Movies(id=4,
+                    title='Rush Hour',
+                    year=1998,
+                    description='When Hong Kong Inspector Lee is summoned to Los Angeles to investigate a kidnapping, the FBI doesn\'t want any outside help and assigns cocky LAPD Detective James Carter to distract Lee from the case.',
+                    rating=7,
+                    ranking=3,
+                    review='My favorite comedy featuring one of my favorite actors, Jackie Chan.',
+                    img_url='https://image.tmdb.org/t/p/w500//5LM4sJ2SYTvo6q3sOdYaL7Ly3uj.jpg')
+    movie5 = Movies(id=5,
+                    title='Your Name',
+                    year=2018,
+                    description='J.J. Abrams and Paramount Pictures have announced they’ll be turning the critically acclaimed Japanese anime Your Name into a live-action film.',
+                    rating=9,
+                    ranking=2,
+                    review='Another amazing movie that came out in the past decade. I highly recommend watching this.',
+                    img_url='https://image.tmdb.org/t/p/w500//y00slgxcvchoc7r3bNiI3UbnKkU.jpg')
+    db.session.add(movie1)
+    db.session.add(movie2)
+    db.session.add(movie3)
+    db.session.add(movie4)
+    db.session.add(movie5)
+    db.session.commit()
 except:
+    print('no')
     pass
 
 # new_movie = Movies(
